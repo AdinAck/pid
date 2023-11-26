c84186f6d02bb08d524d9a09f1db99be90d21dd2
Adin Ackerman <adinackerman@gmail.com>
2023-11-25 16:32:58 -0800

    export pidterm trait

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,11 @@ use num_traits::{
     clamp, CheckedAdd, CheckedDiv, CheckedMul, CheckedNeg, CheckedSub, Num, One, Zero,
 };
 
+pub trait PIDTerm:
+    Num + Ord + CheckedAdd + CheckedSub + CheckedMul + CheckedDiv + CheckedNeg + Copy
+{
+}
+
 pub struct PIDController<T> {
     k_p: T,
     k_i: T,
@@ -17,7 +22,7 @@ pub struct PIDController<T> {
 
 impl<T> PIDController<T>
 where
-    T: Num + Ord + CheckedAdd + CheckedSub + CheckedMul + CheckedDiv + CheckedNeg + Copy,
+    T: PIDTerm,
 {
     pub fn new(k_p: T, k_i: T, k_d: T, windup_limit: T, divisor: T) -> Self {
         Self {
